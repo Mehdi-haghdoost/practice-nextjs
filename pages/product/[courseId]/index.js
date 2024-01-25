32ddd0b58b7de8b50d53d9473cf289fc5bcd35d1
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-01-25 20:09:09 +0330

    Create Dynamic Nested Routes

---
+++ pages/product/[courseId]/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() { 
+  return (
+    <h1>12</h1>
+  )
+}
+
+export default index
--- pages/product/node-js.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Nodejs() {
-  return (
-    <h1>Nodejs Course</h1>
-  )
-}
-
-export default Nodejs
