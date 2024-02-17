93fd5dbc5f323e548c1e331864bdf9a0554ad08a
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-02-17 13:19:16 +0330

    coding dynamic routes with getStatisProps method

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from '../../styles/[id].module.css'
 
 function product({ product }) {
     return (
@@ -6,16 +7,33 @@ function product({ product }) {
             <h2>
                 {product.title}
             </h2>
-            ho
+            <img className={styles.product_Img} src={product.image} alt="" />
+            <hr />
         </div>
     )
 }
 
-export async function getStaticPath(context) {
+export async function getStaticPaths() {
+
+    return {
+        paths: [
+            { params: { id: '1' } },
+            { params: { id: '2' } },
+            { params: { id: '3' } },
+            { params: { id: '4' } },
+            { params: { id: '5' } },
+            { params: { id: '6' } },
+            { params: { id: '7' } },
+        ],
+        fallback: false
+    };
+}
+
+export async function getStaticProps(context) {
     const productID = context.params.id
     const res = await fetch(`https://fakestoreapi.com/products/${productID}`)
     const data = await res.json()
-    
+
     return {
         props: {
             product: data,
