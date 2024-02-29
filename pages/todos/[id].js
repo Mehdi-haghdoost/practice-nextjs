0bb9da5f622ee9d6bcac371a0be7b14aadbd3b49
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-02-29 18:51:22 +0330

    csr real example

--- pages/todos/[id].js
+++ pages/todos/[id].js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Todo() {
-  return (
-    <div>Todo</div>
-  )
+function Todo({ id, completed, title }) {
+    const [todo, setTodo] = useState({ id, completed, title })
+
+    async function toggleCompleteHandler() {
+
+        const res = await fetch(`http://localhost:4000/todos/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: todo.title,
+                completed: !todo.completed,
+            }),
+        })
+        console.log('res =>', res);
+        const data = await res.json()
+        if (res.status === 200) {
+            setTodo(data)
+            console.log("Todo Completed Successfully :))");
+        }
+    }
+
+    return (
+        <li key={todo.id}>
+
+            <h3>{todo.id}.{todo.title}</h3>
+            <p>
+                completed : {todo.completed ? "✅" : "❌"}
+            </p>
+
+            <button onClick={() => {
+                toggleCompleteHandler()
+            }}>toggle completed</button>
+            <hr />
+        </li>
+    )
 }
 
 export default Todo
--- pages/todos/index.js
+++ pages/todos/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
+import Todo from './[id]';
 
 function Todos() {
 
@@ -9,25 +9,17 @@ function Todos() {
         fetch('http://localhost:4000/todos')
             .then(res => res.json())
             .then(data => setTodos(data))
+        
     }, [])
 
     return (
         <ul>
             {todos.length ? (
                 todos.map((todo) => (
-                    <li key={todo.id}>
-                        <Link href={`/todos/${todo.id}`}>
-                            <h3>{todo.id}.{todo.title}</h3>
-                        </Link>
-                        <hr />
-                        <p>
-                            completed : {todo.completed ? "✅" : "❌"}
-                        </p>
-                    </li>
-                    
+                   <Todo key={todo.id} {...todo} />
                 ))
-            ): <h1>Todos notFound</h1>
-        }
+            ) : <h1>Todos notFound</h1>
+            }
         </ul >
     )
 }
