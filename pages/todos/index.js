b2471a0d4a392084695002996cf89e6310b3aaa8
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-03-02 11:19:51 +0330

    use SWR for data fetching

--- pages/todos/index.js
+++ pages/todos/index.js
@@ -1,30 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import useSWR from 'swr'
+
+const todosFetcher = () => 
+    fetch("http://localhost:4000/todos").then(res => res.json());
+
+
 function Todos() {
 
-    const [todos, setTodos] = useState([]);
+    const { data, error } = useSWR("todos", todosFetcher)
+    console.log(data);
+    if (error) {
+        return <h1>Unknow Error !!</h1>
+    }
 
-    useEffect(() => {
-        fetch('http://localhost:4000/todos')
-            .then(res => res.json())
-            .then(data => setTodos(data))
-        
-    }, [])
+
+
 
     return (
         <ul>
-            {todos.length ? (
-                todos.map((todo) => (
-                   <li key={todo.id}>
-                    <Link href={`/todos/${todo.id}`}>
-                        <h3>
-                            {todo.id}- {todo.title}
-                        </h3>
-                        <hr />
-                    </Link>
-                   </li>
+            {data?.length ? (
+                data?.map((todo) => (
+                    <li key={todo.id}>
+                        <Link href={`/todos/${todo.id}`}>
+                            <h3>
+                                {todo.id}- {todo.title}
+                            </h3>
+                            <hr />
+                        </Link>
+                    </li>
                 ))
-            ) : <h1>Todos notFound</h1>
+            ) : null
             }
         </ul >
     )
