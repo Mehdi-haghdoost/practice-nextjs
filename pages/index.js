5b79134146bf719f8d495a616f0eaa168e8cdf84
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-03-23 11:11:06 +0330

    fetchUsers from api

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
-import Link from "next/link";
-
+import { useState } from "react";
 export default function Home() {
+  const [users, setUsers] = useState([])
+
+  const fetchUsers = async () => {
+    const res = await fetch('/api/users')
+    const data = await res.json()
+    setUsers(data
+
+    )
+    console.log('data =>', data);
+  }
+
+
   return (
     <>
-   <div>hello</div>
+      <button onClick={fetchUsers}>load users</button>
+      <br /><br />
+      <h3>Users :</h3>
+      <ul>
+        {
+          users.map((user) => (
+            <li key={user.id}> {user.username} </li>
+          ))
+        }
+      </ul>
     </>
   );
 }
