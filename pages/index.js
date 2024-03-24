923a65e0903b1d1045f05f1df3ebb52702b28bc5
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-03-24 18:49:08 +0330

    use Formik for Validation

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,78 @@
-import { useState } from "react";
+import React from "react";
+import { useFormik } from "formik";
 
 
 export default function Home() {
 
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const form = useFormik({
+    initialValues: { Username: '', email: '', password: '' },
+
+    onSubmit: (values, { setSubmitting }) => {
+      console.log("Form Inputs Data =>", values);
+      setTimeout(() => {
+        setSubmitting(false);
+      }, 3000);
+    },
+
+    validate: (values) => {
+      const errors = {};
+
+      if (values.Username === "") {
+        errors.Username = "وارد کردن نام اجباری می‌باشد";
+      } else if (values.Username.length < 4) {
+        errors.Username = "طول نام حداقل باید ۴ کاراکتر باشد";
+      }
+
+      if (values.email === "") {
+        errors.email = "وارد کردن ایمیل اجباری می‌باشد";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      ) {
+        errors.email = "ایمیل وارد شده معتبر نیست";
+      }
+
+      if (values.password === "") {
+        errors.password = "وارد کردن پسورد اجباری می‌باشد";
+      } else if (values.password.length < 4) {
+        errors.password = "پسورد حداقل باید ۴ کاراکتر باشد";
+      }
+
+      return errors;
+    },
+
+
+
+  })
 
   const registerNewUser = async (event) => {
     event.preventDefault()
 
     const newUser = {
-      username,
-      email,
-      password,
+      username: form.values.Username,
+      email: form.values.email,
+      password: form.values.password,
     }
+    console.log('newUser =>' , newUser);
 
-    const res = await fetch('/api/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newUser)
-    })
+    // const res = await fetch('/api/users', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(newUser)
+    // })
 
-    const data = await res.json();
+    // const data = await res.json();
 
 
-    console.log('Response =>', res);
-    console.log("response data =>", data);
+    // console.log('Response =>', res);
+    // console.log("response data =>", data);
 
-    if (res.status === 201) {
-      setUsername('')
-      setEmail('')
-      setPassword('')
-    }
+    // if (res.status === 201) {
+    //   setUsername('')
+    //   setEmail('')
+    //   setPassword('')
+    // }
   }
 
   return (
@@ -44,11 +82,56 @@ export default function Home() {
         <h1>Creative SignUp Form</h1>
         <div className="main-agileinfo">
           <div className="agileits-top">
-            <form action="#" method="post">
-              <input className="text" value={username} onChange={(event) => setUsername(event.target.value)} type="text" name="Username" placeholder="Username" />
-              <input className="text email" value={email} onChange={(event) => setEmail(event.target.value)} type="email" name="email" placeholder="Email" />
-              <input className="text" value={password} onChange={(event) => setPassword(event.target.value)} type="password" name="password" placeholder="Password" />
-              <button type="submit" onClick={registerNewUser}>Register</button>
+            <form onSubmit={form.handleSubmit}>
+              <input
+                className="text"
+                type="text"
+                name="Username"
+                placeholder="Username"
+                value={form.values.Username}
+                onBlur={form.handleBlur}
+                onChange={form.handleChange}
+              />
+              {
+                form.errors.Username && form.touched.Username && form.errors.Username
+              }
+
+              <input
+                className="text email"
+                type="email"
+                name="email"
+                placeholder="Email"
+                onBlur={form.handleBlur}
+                onChange={form.handleChange}
+                value={form.values.email}
+              />
+              {
+                form.errors.email && form.touched.email && form.errors.email
+              }
+
+
+              <input
+                className="text"
+                type="password"
+                name="password"
+                placeholder="Password"
+                onBlur={form.handleBlur}
+                onChange={form.handleChange}
+                value={form.values.password}
+              />
+              {
+                form.errors.password && form.touched.password && form.errors.password
+              }
+
+              <button type="submit"
+              onSubmit={ registerNewUser}
+                className={form.isSubmitting ? 'invalid-btn' : ''}
+                disabled={form.isSubmitting}
+              >
+                {
+                  form.isSubmitting ? "Loading ..." : "Register"
+                }
+              </button>
             </form>
           </div>
 
